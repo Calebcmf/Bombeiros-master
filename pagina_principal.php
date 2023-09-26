4ddc71aa0299484e3ee07285621ec607589aff54
CalebCMF <caleb_freitas@estudante.sesisenai.org.br>
2023-09-26 08:18:23 -0300

    push

--- pagina_principal.php
+++ pagina_principal.php
@@ -9,13 +9,39 @@
 </head>
 <body>
     <div class="menu">
-        <a class="opition" href="#dados_paciente">Dados do Paciente</a>
-        <a class="opition" href="#tipo_ocorrencia">Tipo de Ocorrência</a>
-        <a class="opition" href="#localizacao_traumas">Localização Traumas</a>
-        <a class="opition" href="#observacoes">Observações Importantes</a>
-        <a class="opition" href="#nivel_consciencia">Nível de Consciencia</a>
-    </div>
+        <div class="pop-up_faleconosco">
+            <h2>Fale Conosco</h2>
+            <form action="duvidas.php" method="post">
+                <label for="nome" >Nome</label>
+                <input type="text" class="nome" id="nome" name="nome" placeholder="Seu nome" required><br>
     
+                <label for="email" >Email</label>
+                <input type="email" class="email" id="email" name="email" placeholder="Seu email" required><br>
+    
+                <label for="mensagem">Mensagem</label>
+                <textarea id="mensagem" class="msg" name="mensagem" onkeypress="return searchKeyPress(event)" placeholder="Digite sua mensagem" required></textarea>
+                <button onclick="Esconder();">Fechar</button>
+                <input type="submit" value="Enviar" name="enviar" class="">
+            </form>
+        </div>
+        <div class="logos">
+            <img src="img/logo_medicina.png" width="40%">
+            <img src="img/logo_pequena.png" width="40%">
+        </div>
+        <div class="opcoes">
+            <a class="opition" href="#dados_paciente">Dados do Paciente</a>
+            <a class="opition" href="#tipo_ocorrencia">Tipo de Ocorrência</a>
+            <a class="opition" href="#localizacao_traumas">Localização Traumas</a>
+            <a class="opition" href="#observacoes">Observações Importantes</a>
+            <a class="opition" href="#nivel_consciencia">Nível de Consciencia</a>
+        </div>
+        <div class="rodape">
+            <div class="fale_conosco">Fale Conosco</div>
+            <div class="ajuda">Ajuda</div>
+            <a href="">Suporte</a>
+        </div>
+    </div>
+
     <div class="principal">
         <div class="pagina" id="dados_paciente">
             <?php 
