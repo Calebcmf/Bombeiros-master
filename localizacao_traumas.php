82522eed762c094f1e137c907298f4f170658374
AntonioBudal <antonio_b_lima@estudante.sesisenai.org.br>
2023-09-26 09:11:49 -0300

    teste

--- localizacao_traumas.php
+++ localizacao_traumas.php
@@ -12,7 +12,7 @@
     
     <div class="cabecalho">
         <div class="titulo">
-            Localização 
+            Localizacao 
         </div>
         <div class="subtitulo">
             dos Traumas
@@ -37,68 +37,72 @@
     </div>
     
 
-   
-    <div class="popUP" id="1">
+    
+    <form class="popUP" id="1">
+    
             <div class="m">
                 <div class="fechar" onclick="fechar(1)">X</div>
-                <img src="img/Cabeca.png" height="50%" id="im" alt=""></img>
+                <img src="img/Cabeca.png" height="60%" id="im" alt=""></img>
                 <p id="mtd" >Cabeça<p>
             </div>
         <div class="m m1">
             <div class="tri">
                 <div class="escolha">
-                    <input type="checkbox" id="check_1">
+                    <input type="checkbox" id="check_1" class = "tra">
                     <label for="check_1">FRA/LUX/ENT</label>    
                 </div>
                 
                 <div class="escolha">
-                    <input type="checkbox" id="check_2">
+                    <input type="checkbox" id="check_2" class = "tra">
                     <label for="check_2">ESVICERAÇÃO</label>  
                 </div>
                 
                 <div class="escolha">
-                    <input type="checkbox" id="check_3">
+                    <input type="checkbox" id="check_3" class = "tra">
                     <label for="check_3">QUEIMADURA</label>  
                 </div>
             </div>
 
             <div class="tri">
                 <div class="escolha">
-                    <input type="checkbox" id="check_4">
+                    <input type="checkbox" id="check_4" class = "tra">
                     <label for="check_4">FERIMENTOS</label>  
                 </div>
 
                 <div class="escolha">
-                    <input type="checkbox" id="check_5">
+                    <input type="checkbox" id="check_5" class = "tra">
                     <label for="check_5">F.A.B/F.A.F</label> 
                 </div>
 
                 <div class="escolha">
-                    <input type="checkbox" id="check_6">
+                    <input type="checkbox" id="check_6" class = "tra">
                     <label for="check_6">QUEIMADURA</label>
                 </div>
             </div>
 
             <div class="tri">
                 <div class="escolha">
-                    <input type="checkbox" id="check_7">
+                    <input type="checkbox" id="check_7" class = "tra">
                     <label for="check_7">HEMARROGIA</label>
                 </div>
                 
                 <div class="escolha">
-                    <input type="checkbox" id="check_8">
+                    <input type="checkbox" id="check_8" class = "tra">
                     <label for="check_8">AMPTUÇÃO</label>
                 </div>
                 
                 <div class="escolha">
-                    <input type="checkbox" id="check_9">
+                    <input type="checkbox" id="check_9" class = "tra">
                     <label for="check_9">QUEIMADURA</label>
                 </div>
             </div>
         </div> 
         
-        <div class="btnF"></div>
-    </div>
+        <input type="submit" value="Enviar" name="entrarR" class="btnF">
+
+        
+    </form> 
+ 
 
         
    
