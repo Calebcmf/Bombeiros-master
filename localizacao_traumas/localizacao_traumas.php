02bfdcce7bfadf9e396e619fcec1e5372617334c
AntonioBudal <antonio_b_lima@estudante.sesisenai.org.br>
2023-10-05 12:43:21 -0300

    Antonio

--- localizacao_traumas/localizacao_traumas.php
+++ localizacao_traumas/localizacao_traumas.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="localizacao_traumas.css">
+    <link rel="stylesheet" href="../localizacao_traumas/localizacao_traumas.css">
    
 </head>
 <body>
@@ -23,67 +23,67 @@
     <div class="container">
             <div class="d1">
                 <div class="d2">
-                    <img src="img/cabeca.png" class="cabeca h">
+                    <img src="../img/cabeca.png" class="cabeca h" onclick="criar(1,'Cabeca')">
                 </div>
                 <div class="d2 c">
-                <img src="img/pescoco.png"       class="pescoco h ">   </img>
+                <img src="../img/pescoco.png"       class="pescoco h " onclick="criar(1,'Pescoco')">   </img>
                 </div>
             </div>
             <div class="d1 r">
                 <div class="d3 b">
-                <img src="img/bicepsdir.png"     class="bicepsD h ">   </img>   
+                <img src="../img/bicepsdir.png"     class="bicepsD h " onclick="criar(1,'bicepsdir')">   </img>   
                 </div>
                 <div class="d3">
-                <img src="img/tricepsdir.png"    class="tricepsD h">  </img>
+                <img src="../img/tricepsdir.png"    class="tricepsD h" onclick="criar(1,'tricepsdir')">  </img>
                 </div>
                 <div class="d3">
-                <img src="img/peitodir.png"      class="peitoD h " >    </img>
+                <img src="../img/peitodir.png"      class="peitoD h " onclick="criar(1,'peitodir')">    </img>
                 </div>
                 <div class="d3">
-                <img src="img/peitoesq.png"      class="peitoE h " >    </img>
+                <img src="../img/peitoesq.png"      class="peitoE h " onclick="criar(1,'peitoesq')">    </img>
                 </div>
                 <div class="d3">
-                <img src="img/tricepsesq.png"    class="tricepsE h">  </img> 
+                <img src="../img/tricepsesq.png"    class="tricepsE h"onclick="criar(1,'tricepsesq')">  </img> 
                 </div>
                 <div class="d3 b">
-                <img src="img/bicepsesq.png"     class="bicepsE h">   </img> 
+                <img src="../img/bicepsesq.png"     class="bicepsE h"onclick="criar(1,'bicepsdir')">   </img> 
                 </div>
             </div>
             <div class="d1 r r2">
             <div class="d3 q">
-                <img src="img/MaoDir.png"    class="maoD h ">  </img>
+                <img src="../img/MaoDir.png"    class="maoD h " onclick="criar(1,'MaoDir')">  </img>
             </div>
                 <div class="d3"></div>
                 <div class="d3">
-                    <img src="img/barrigadir.png"    class="barrigaD h ">  </img>
+                    <img src="../img/barrigadir.png"    class="barrigaD h " onclick="criar(1,'barrigadir')">  </img>
                 </div>
                 <div class="d3">
-                <img src="img/barrigaesq.png"    class="barrigaE h ">  </img>
+                <img src="../img/barrigaesq.png"    class="barrigaE h " onclick="criar(1,'barrigaesq')">  </img>
                 </div>
                 <div class="d3"></div>
             <div class="d3 q ">
-                <img src="img/MaoEsq.png"    class="maoE h ">  </img>
+                <img src="../img/MaoEsq.png"    class="maoE h " onclick="criar(1,'maoesq')">  </img>
             </div>
             </div>
             <div class="d1 cx">
                 <div class="d2 c">
-                <img src="img/pelvis.png"        class="pelvis h " >    </img>
+                <img src="../img/pelvis.png"        class="pelvis h " onclick="criar(1,'pelvis')" >    </img>
                 </div>
                 <div class="d2 s">
                 <div class="d3 v ">
-                <img src="img/coxaesq.png"       class="coxaE h "  >     </img>
+                <img src="../img/coxaesq.png"       class="coxaE h " onclick="criar(1,'coxaesq')" >     </img>
                 </div>
                 <div class="d3 v ">
-                <img src="img/coxadir.png"       class="coxaD h "  >     </img>
+                <img src="../img/coxadir.png"       class="coxaD h "  onclick="criar(1,'coxadir')">     </img>
                 </div>
                 </div>
             </div>
             <div class="d1 r">
                 <div class="d4">
-                <img src="img/pernaesq.png" class="pernaE h "  > </img>
+                <img src="../img/pernaesq.png" class="pernaE h "   onclick="criar(1,'pernaesq')"> </img>
                 </div>
                 <div class="d4">
-                <img src="img/pernadir.png"class="pernaD h "  >     </img>
+                <img src="../img/pernadir.png"class="pernaD h "  onclick="criar(1,'pernadir')" >     </img>
                 </div>
             </div>
 
@@ -93,28 +93,14 @@
                 </div>
                 <div class="d2">
                 <div class="d4 s">
-                    <img src="img/pedir.png"         class="peD h "    >       </img>
+                    <img src="../img/pedir.png"         class="peD h "     onclick="criar(1,'pedir')">       </img>
                     </div>
                     <div class="d4 s ">
-                    <img src="img/peesq.png"         class="peE h"    >       </img> 
+                    <img src="../img/peesq.png"         class="peE h"     onclick="criar(1,'peesq')">       </img> 
                     </div>
                 </div>
             </div>
-            <!-- <img src="img/cabeca.png"        class="cabeca h">    </img>
-            <img src="img/pescoco.png"       class="pescoco h ">   </img>
-            <img src="img/tricepsdir.png"    class="tricepsD h">  </img>
-            <img src="img/tricepsesq.png"    class="tricepsE h">  </img> 
-            <img src="img/bicepsdir.png"     class="bicepsD h ">   </img>
-            <img src="img/bicepsesq.png"     class="bicepsE h">   </img> 
-            <img src="img/peitodir.png"      class="peitoD h " >    </img>
-            <img src="img/peitoesq.png"      class="peitoE h " >    </img> 
-            <img src="img/barrigadir.png"    class="barrigaD h ">  </img>
-            <img src="img/barrigaesq.png"    class="barrigaE h ">  </img>
-            <img src="img/pelvis.png"        class="pelvis h " >    </img>
-            <img src="img/coxaesq.png"       class="coxaE h "  >     </img>
-            <img src="img/coxadir.png"       class="coxaD h "  >     </img>
-            <img src="img/pedir.png"         class="peD h "    >       </img>
-            <img src="img/peesq.png"         class="peE h"    >       </img>       -->
+            
         </div>  
     </div>
 
@@ -126,7 +112,10 @@
     
             <div class="m">
                 <div class="fechar" onclick="fechar(1)">X</div>
-                <img src="img/Cabeca.png" height="60%" id="im" alt=""></img>
+                <div class="quadI">
+                    <img src="../img/Cabeca.png" width  ="50%" height="auto" id="im" alt=""></img>
+                </div>
+                
                 <p id="mtd" >Cabeça<p>
             </div>
         <div class="m m1">
@@ -195,5 +184,5 @@
         
    
 </body>
-    <script src="localizacao_traumas.js"> </script>
+    <script src="../localizacao_traumas/localizacao_traumas.js"> </script>
 </html>
