c332ea020b88013eb8cc23da2e67fff4177b3e8f
CalebCMF <caleb_freitas@estudante.sesisenai.org.br>
2023-11-09 11:13:18 -0300

    sync

---
+++ login_adm/login_adm.php
@@ -0,0 +1,18 @@
+<?php
+include("../conecta.php");
+
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+$password = md5($senha);
+
+$comando = $pdo->prepare("SELECT * FROM cadastro_admin WHERE email = '$email' and senha = '$password'");
+$resultado = $comando->execute();
+$n = $comando->rowCount();
+if ($n > 0) {
+    // Email e senha corretos
+    echo "success"; // Envia uma resposta de sucesso para o AJAX
+} else {
+    // Senha ou email incorretos
+    echo "error"; // Envia uma resposta de erro para o AJAX
+}
+?>
