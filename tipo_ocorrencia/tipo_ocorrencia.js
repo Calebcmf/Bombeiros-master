450a24f4a07053c6db7af49e2230342a2cbe0b41
AntonioBudal <antonio_b_lima@estudante.sesisenai.org.br>
2023-11-16 12:44:54 -0300

    Arrumando Css

--- tipo_ocorrencia/tipo_ocorrencia.js
+++ tipo_ocorrencia/tipo_ocorrencia.js
@@ -5,7 +5,7 @@
 
     for(var i=0; i<children.length; i++){
         
-            var teste = document.getElementById(`check_${i}`).innerHTML;
+            var teste = document.getElementById(`var_${i}`).innerHTML;
             texto[i] = teste;
             console.log(texto[i]);
     }
@@ -15,25 +15,45 @@
     const divs = [];
 
     for (var i = 0; i < children.length; i++) {
-        var teste = document.getElementById(`check_${i}`);
+        var teste = document.getElementById(`var_${i}`);
         divs.push(teste);
     }
 
     let filterUsers = function (event) {
+
+
         keyword = input.value.toLowerCase();
         for (var i = 0; i < divs.length; i++) {
+          
             var divText = divs[i].textContent.toLowerCase();
             if (divText.includes(keyword)) {
                 divs[i].style.display = 'flex';
+               
                 divs[i].parentNode.style.display = 'flex'; // Show the div
+              
+             
             } else {
                 divs[i].style.display = 'none';
                 divs[i].parentNode.style.display = 'none';  // Hide the div
+         
             }
+            let width = screen.width;
+            console.log(width);
+            if(width < 600){
+                element.style.paddingTop = "240%"
+            } else{
+                element.style.paddingTop = "35%"
+            }
+            
         }
+
+        
     }
 
+
     input.addEventListener('keyup', filterUsers);
+   
+   
 
         
     
